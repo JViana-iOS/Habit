28fce8cddec2a1bef3a9492725f156f034d183a5
JViana-iOS <jeanviana18@icloud.com>
2024-11-21 15:39:49 -0300

    Initial Commit

---
+++ Habit/HabitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitApp.swift
+//  Habit
+//
+//  Created by Jean Viana on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HabitTests/HabitTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTests.swift
+//  HabitTests
+//
+//  Created by Jean Viana on 21/11/24.
+//
+
+import Testing
+@testable import Habit
+
+struct HabitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
